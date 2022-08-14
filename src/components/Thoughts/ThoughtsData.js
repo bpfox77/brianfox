403da8e6129af9e970781eeefdf3634213fc4aee
bpfox77 <56087137+bpfox77@users.noreply.github.com>
2022-08-14 16:55:53 -0500

    price

--- src/components/Thoughts/ThoughtsData.js
+++ src/components/Thoughts/ThoughtsData.js
@@ -2,340 +2,340 @@ export const ThoughtsData = [
   {
     url: 'Nfts/1_Apes.svg',
     name: 'Apes',
-    price: '1',
+    price: '.7',
     number: '01',
   },
   {
     url: 'Nfts/2_Photons.svg',
     name: 'Photons',
-    price: '1',
+    price: '.7',
     number: '02',
   },
   {
     url: 'Nfts/3_Antennae.svg',
     name: 'Antennae',
-    price: '1',
+    price: '.7',
     number: '03',
   },
   {
     url: 'Nfts/4_Clues.svg',
     name: 'Clues',
-    price: '1',
+    price: '.7',
     number: '04',
   },
 
   {
     url: 'Nfts/5_Atoms.svg',
     name: 'Atoms',
-    price: '1',
+    price: '.7',
     number: '05',
   },
 
   {
     url: 'Nfts/6_ECCO.svg',
     name: 'E.C.C.O.',
-    price: '1',
+    price: '.7',
     number: '06',
   },
 
   {
     url: 'Nfts/7_Laboratories.svg',
     name: 'Laboratories',
-    price: '1',
+    price: '.7',
     number: '07',
   },
 
   {
     url: 'Nfts/8_Downloads.svg',
     name: 'Downloads',
-    price: '1',
+    price: '.7',
     number: '08',
   },
 
   {
     url: 'Nfts/9_Dreams.svg',
     name: 'Dreams',
-    price: '1',
+    price: '.7',
     number: '09',
   },
 
   {
     url: 'Nfts/10_Radar.svg',
     name: 'Radar',
-    price: '1',
+    price: '.7',
     number: '10',
   },
 
   {
     url: 'Nfts/11_Beacons.svg',
     name: 'Beacons',
-    price: '1',
+    price: '.7',
     number: '11',
   },
 
   {
     url: 'Nfts/12_Reality.svg',
     name: 'Reality',
-    price: '1',
+    price: '.7',
     number: '12',
   },
 
   {
     url: 'Nfts/13_Riddles.svg',
     name: 'Riddles',
-    price: '1',
+    price: '.7',
     number: '13',
   },
 
   {
     url: 'Nfts/14_Fractals.svg',
     name: 'Fractals',
-    price: '1',
+    price: '.7',
     number: '14',
   },
 
   {
     url: 'Nfts/15_Linearity.svg',
     name: 'Linearity',
-    price: '1',
+    price: '.7',
     number: '15',
   },
 
   {
     url: 'Nfts/16_Bits.svg',
     name: 'Bits',
-    price: '1',
+    price: '.7',
     number: '16',
   },
 
   {
     url: 'Nfts/17_Stargate.svg',
     name: 'Stargate',
-    price: '1',
+    price: '.7',
     number: '17',
   },
 
   {
     url: 'Nfts/18_Entangled.svg',
     name: 'Entangled',
-    price: '1',
+    price: '.7',
     number: '18',
   },
 
   {
     url: 'Nfts/19_Threshold.svg',
     name: 'Threshold',
-    price: '1',
+    price: '.7',
     number: '19',
   },
 
   {
     url: 'Nfts/20_Vessels.svg',
     name: 'Vessels',
-    price: '1',
+    price: '.7',
     number: '20',
   },
 
   {
     url: 'Nfts/21_Magic.svg',
     name: 'Magic',
-    price: '1',
+    price: '.7',
     number: '21',
   },
 
   {
     url: 'Nfts/22_Simulations.svg',
     name: 'Simulations',
-    price: '1',
+    price: '.7',
     number: '22',
   },
 
   {
     url: 'Nfts/23_Entities.svg',
     name: 'Entities',
-    price: '1',
+    price: '.7',
     number: '23',
   },
 
   {
     url: 'Nfts/24_Upgrades.svg',
     name: 'Upgrades',
-    price: '1',
+    price: '.7',
     number: '24',
   },
 
   {
     url: 'Nfts/25_Alchemy.svg',
     name: 'Alchemy',
-    price: '1',
+    price: '.7',
     number: '25',
   },
 
   {
     url: 'Nfts/26_Indra.svg',
     name: 'Indra',
-    price: '1',
+    price: '.7',
     number: '26',
   },
 
   {
     url: 'Nfts/27_Synthesis.svg',
     name: 'Synthesis',
-    price: '1',
+    price: '.7',
     number: '27',
   },
 
   {
     url: 'Nfts/28_Engine.svg',
     name: 'Engine',
-    price: '1',
+    price: '.7',
     number: '28',
   },
 
   {
     url: 'Nfts/29_Traceroute.svg',
     name: 'Traceroute',
-    price: '1',
+    price: '.7',
     number: '29',
   },
 
   {
     url: 'Nfts/30_Flowing.svg',
     name: 'Flowing',
-    price: '1',
+    price: '.7',
     number: '30',
   },
 
   {
     url: 'Nfts/31_Cathedral.svg',
     name: 'Cathedral',
-    price: '1',
+    price: '.7',
     number: '31',
   },
 
   {
     url: 'Nfts/32_Gateless.svg',
     name: 'Gateless',
-    price: '1',
+    price: '.7',
     number: '32',
   },
 
   {
     url: 'Nfts/33_Avatars.svg',
     name: 'Sages',
-    price: '1',
+    price: '.7',
     number: '33',
   },
 
   {
     url: 'Nfts/34_Surrender.svg',
     name: 'Surrender',
-    price: '1',
+    price: '.7',
     number: '34',
   },
 
   {
     url: 'Nfts/35_Nectar.svg',
     name: 'Nectar',
-    price: '1',
+    price: '.7',
     number: '35',
   },
 
   {
     url: 'Nfts/36_Now.svg',
     name: 'Now',
-    price: '1',
+    price: '.7',
     number: '36',
   },
 
   {
     url: 'Nfts/37_Self.svg',
     name: 'Self',
-    price: '1',
+    price: '.7',
     number: '37',
   },
 
   {
     url: 'Nfts/38_Symbols.svg',
     name: 'Symbols',
-    price: '1',
+    price: '.7',
     number: '38',
   },
 
   {
     url: 'Nfts/39_Ceremony.svg',
     name: 'Ceremony',
-    price: '1',
+    price: '.7',
     number: '39',
   },
 
   {
     url: 'Nfts/40_Metamorphis.svg',
     name: 'Chrysalis',
-    price: '1',
+    price: '.7',
     number: '40',
   },
 
   {
     url: 'Nfts/41_Resurrection.svg',
     name: 'Resurrection',
-    price: '1',
+    price: '.7',
     number: '41',
   },
 
   {
     url: 'Nfts/42_Atman.svg',
     name: 'Atman',
-    price: '1',
+    price: '.7',
     number: '42',
   },
 
   {
     url: 'Nfts/43_Divergence.svg',
     name: 'Divergence',
-    price: '1',
+    price: '.7',
     number: '43',
   },
 
   {
     url: 'Nfts/44_Cipher.svg',
     name: 'Cipher',
-    price: '1',
+    price: '.7',
     number: '44',
   },
 
   {
     url: 'Nfts/45_Gnosis.svg',
     name: 'Gnosis',
-    price: '1',
+    price: '.7',
     number: '45',
   },
 
   {
     url: 'Nfts/46_Transcribe.svg',
     name: 'Transcribe',
-    price: '1',
+    price: '.7',
     number: '46',
   },
 
   {
     url: 'Nfts/47_Readouts.svg',
     name: 'Readouts',
-    price: '1',
+    price: '.7',
     number: '47',
   },
 
   {
     url: 'Nfts/48_Equanimity.svg',
     name: 'Equanimity',
-    price: '1',
+    price: '.7',
     number: '48',
   },
 
   {
     url: 'Nfts/49_Perception.svg',
     name: 'Perception',
-    price: '1',
+    price: '.7',
     number: '49',
   },
 ];
